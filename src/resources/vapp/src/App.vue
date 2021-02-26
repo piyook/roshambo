2984eaff79fd4e6a67185d98a1bd0f42bca07f5b
piyook <piyook@piyook.com>
2021-02-26 15:37:44 +0000

    ADD PAGE TRANSITION ANIMATION

--- src/resources/vapp/src/App.vue
+++ src/resources/vapp/src/App.vue
@@ -1,7 +1,11 @@
 <template>
 <div>
   <header-comp> </header-comp>
-  <router-view> </router-view>
+  <router-view v-slot="slotProps">
+    <transition name="route" mode="out-in">
+      <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
 </div>
 </template>
 
@@ -18,6 +22,28 @@ export default {
 <style>
 div #nav {
   text-align: right;
+}
+
+.route-enter-from {
+  opacity: 0;
+}
+.route-enter-active {
+  transition: all 0.4s ease-in;
+}
+
+.route-leave-active {
+  transition: all 0.4s ease-out;
+}
+
+.route-enter-to {
+  opacity: 1;
+}
+
+.route-leave-to {
+  opacity: 0;
+}
+.route-leave-from {
+  opacity: 1;
 }
 
 </style>
