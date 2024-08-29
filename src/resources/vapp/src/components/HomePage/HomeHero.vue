da35c64c66856991906d87b849c8e94a75b91aa0
piyook <piyook@piyook.com>
2024-08-29 09:30:58 +0100

    chore(): add load spinner on homepage play now action

--- src/resources/vapp/src/components/HomePage/HomeHero.vue
+++ src/resources/vapp/src/components/HomePage/HomeHero.vue
@@ -1,43 +1,42 @@
 <template>
 
-<section class="text-center p-4 mt-4 border border-primary">
+    <section class="text-center p-4 mt-4 border border-primary">
 
-    <h1 class="display-5 text-primary">Rock, Paper, Scissors, Lizard, Spock</h1>
-    <div class="display-4">
-        A Simple Vue/Laravel Demo App
-    </div>
+        <h1 class="display-5 text-primary">Rock, Paper, Scissors, Lizard, Spock</h1>
+        <div class="display-4">
+            A Simple Vue/Laravel Demo App
+        </div>
 
-    <button 
-        type="button" 
-        class="btn btn-primary btn-lg m-3" 
-        @click.prevent="login"
-    >
-    Play now 
-    </button>
+        <button type="button" class="btn btn-primary btn-lg m-3" @click.prevent="login">
+            Play now
+        </button>
 
-</section>
-    
+    </section>
+
 </template>
 
 <script>
-
+import { Spinner } from '@/utils/spinner.js';
+import { onUnmounted } from 'vue';
 
 export default {
 
-    methods:{
-        login(){
-            this.$router.push({ name: 'Authorized'});
+
+    methods: {
+        login() {
+            Spinner(true);
+            this.$router.push({ name: 'Authorized' });
+        },
+        onUnmounted() {
+            Spinner(false);
         }
     }
-    
+
 }
 </script>
 
 <style scoped>
-
 button {
-        border-radius:10px;
+    border-radius: 10px;
 }
-
 </style>
-
